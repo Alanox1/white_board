d6e96215d4084703de83076837ca22ade76640db
Sofia Bargues <barguessofia@gmail.com>
2024-04-11 15:29:06 +0100

    21:04 min

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import AddTodo from "@/components/shared/AddTodo";
+
 const Home = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <span className="text-3xl font-extrabold uppercase">To-do-app</span>
+      <h1 className="text-3xl font-extrabold uppercase mb-5">
+        Next.js 14 <span className="text-orange-700 ml-2">Server Actions</span>
+      </h1>
+
+      <div className="flex justify-center items-center flex-col w-[1000px]">
+        <AddTodo />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
