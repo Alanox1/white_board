c07a47f0061058ece5ea6f36abcfaaad502a7d66
Sofia Bargues <barguessofia@gmail.com>
2024-04-25 05:47:39 +0100

    messege

--- components/shared/AddTodo.tsx
+++ components/shared/AddTodo.tsx
@@ -4,36 +4,31 @@ import Button from "../ui/Button";
 import { create } from "@/app/actions/todoActions";
 import { TodoType } from "@/types/todoType";
 
-const AddTodo = ({changeOpen}) => {
+const AddTodo = ({ changeOpen }) => {
+  const handleSubmit = () => changeOpen(false);
 
-  const handleSubmit = () => changeOpen(false)
-  
   return (
     <Form action={create} className="w-1/2 m-auto" onSubmit={handleSubmit}>
-       <div className="flex flex-col gap-4 ">
+      <div className="flex flex-col gap-4 ">
         <Input name="input" type="text" placeholder="Add Todo..." />
-        
-
-        <Input name="message" type="text" placeholder="Add Message..."/>  {/*  Input para agregar un mensaje, anda pero sale subrayado en rojo en page.tsx y en todoActions.tsx */}
-
+        <Input name="message" type="text" placeholder="Add Message..." />{" "}
+        {/*  Input para agregar un mensaje, anda pero sale subrayado en rojo en page.tsx y en todoActions.tsx */}
         <div className="flex justify-center gap-10 text-white text-xl">
-            {" "}
-            <label>
-              <Input type="radio" name="backgroundColor" value="blue" />
-              Azul
-            </label>
-            <label>
-              <Input type="radio" name="backgroundColor" value="yellow" />
-              Amarillo
-            </label>
-            <label>
-              <Input type="radio" name="backgroundColor" value="green" />
-              Verde
-            </label>
-          </div>
-
-          
-        <Button type="submit" text="Add"  />
+          {" "}
+          <label>
+            <Input type="radio" name="backgroundColor" value="blue" />
+            Azul
+          </label>
+          <label>
+            <Input type="radio" name="backgroundColor" value="yellow" />
+            Amarillo
+          </label>
+          <label>
+            <Input type="radio" name="backgroundColor" value="green" />
+            Verde
+          </label>
+        </div>
+        <Button type="submit" text="Add" />
       </div>
     </Form>
   );
