0820920e48ee0241ba6c1fb51de59671aec7c264
Alan Magliolini <alannm54@gmail.com>
2024-04-27 00:53:16 -0300

    Cambie un componente que no lo usabamos

--- components/shared/ChangeTodo.tsx
+++ components/shared/ChangeTodo.tsx
@@ -1,22 +1,22 @@
-import Form from "../ui/Form";
-import Button from "../ui/Button";
-import Input from "../ui/Input";
-import { AiOutlineCheckCircle } from "react-icons/ai";
-import { changeStatus } from "@/app/actions/todoActions";
-import { TodoType } from "@/types/todoType";
+// import Form from "../ui/Form";
+// import Button from "../ui/Button";
+// import Input from "../ui/Input";
+// import { AiOutlineCheckCircle } from "react-icons/ai";
+// import { changeStatus } from "@/app/actions/todoActions";
+// import { TodoType } from "@/types/todoType";
 
-const ChangeTodo = ({ todo }: { todo: TodoType }) => {
-  return (
-    <Form action={changeStatus}>
-      <Input name="inputId" value={todo.id} type="hidden" />
-      <Button
-        actionButton
-        type="submit"
+// const ChangeTodo = ({ todo }: { todo: TodoType }) => {
+//   return (
+//     <Form action={changeStatus}>
+//       <Input name="inputId" value={todo.id} type="hidden" />
+//       <Button
+//         actionButton
+//         type="submit"
 
-        // text={<AiOutlineCheckCircle />}
-      />
-    </Form>
-  );
-};
+//         // text={<AiOutlineCheckCircle />}
+//       />
+//     </Form>
+//   );
+// };
 
-export default ChangeTodo;
+// export default ChangeTodo;
